8b586b5e5a2ccafc4f863d2d00d96ce376ba2df0
hwlei888 <leihuawen888@gmail.com>
2024-08-09 01:04:48 +1000

    create some components & add routes"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
+import Navbar from './components/Layout/Navbar';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AddProject from './components/Project/AddProject';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Dashboard />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route exact path="/dashboard" element={<Dashboard />}/>
+            <Route exact path="/addProject" element={<AddProject />}/>
+          </Routes>
+        </div>
+      </Router>
     );
   }
 }
