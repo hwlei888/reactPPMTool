4bc958639224b90bc5491575d4ebb45dafe65cac
hwlei888 <leihuawen888@gmail.com>
2024-11-20 02:32:52 +1100

    make the frontend can update and show the updates

--- src/components/Project/UpdateProject.js
+++ src/components/Project/UpdateProject.js
@@ -1,20 +1,74 @@
 import React, { Component } from 'react'
-import { getProject } from '../../actions/projectActions';
+import { getProject, createProject } from '../../actions/projectActions';
 import PropTypes from "prop-types";
 import { connect } from 'react-redux';
 import classNames from 'classnames';
 import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 
-
-
 class UpdateProject extends Component {
-    componentDidMount() {
+
+    constructor(){
+        super(); //use super here because extending from Component
+
+        this.state = {
+            id: "",
+            projectName: "",
+            projectIdentifier: "",
+            description: "",
+            start_date: "",
+            end_date: "",
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps){
+        const {
+            id,
+            projectName,
+            projectIdentifier,
+            description,
+            start_date,
+            end_date,
+        } = nextProps.project;
+
+        this.setState({
+            id,
+            projectName,
+            projectIdentifier,
+            description,
+            start_date,
+            end_date,
+        })
+    }
+
+    componentDidMount(){
         const { id } = this.props;
         this.props.getProject(id, this.props.navigate);
     }
 
-    render() {
+    onChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value});
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault(); //it will not refresh the page
+
+        const updateProject = {
+            id: this.state.id,
+            projectName: this.state.projectName,
+            projectIdentifier: this.state.projectIdentifier,
+            description: this.state.description,
+            start_date: this.state.start_date,
+            end_date: this.state.end_date,
+        };
+
+        this.props.createProject(updateProject, this.props.navigate);
+    }
+
+    render(){
         return (
             <div className="project">
                 <div className="container">
@@ -22,25 +76,57 @@ class UpdateProject extends Component {
                         <div className="col-md-8 m-auto">
                             <h5 className="display-4 text-center">Update Project form</h5>
                             <hr />
-                            <form>
+                            <form onSubmit={this.onSubmit}>
                                 <div className="form-group">
-                                    <input type="text" className="form-control form-control-lg " placeholder="Project Name" />
+                                    <input 
+                                        type="text" 
+                                        className="form-control form-control-lg " 
+                                        placeholder="Project Name"
+                                        name='projectName'
+                                        value={this.state.projectName}
+                                        onChange={this.onChange}
+                                    />
                                 </div>
                                 <div className="form-group">
-                                    <input type="text" className="form-control form-control-lg" placeholder="Unique Project ID"
-                                        disabled />
+                                    <input 
+                                        type="text" 
+                                        className="form-control form-control-lg" 
+                                        placeholder="Unique Project ID"
+                                        name='projectIdentifier'
+                                        value={this.state.projectIdentifier}
+                                        onChange={this.onChange}
+                                        disabled 
+                                    />
                                 </div>
 
                                 <div className="form-group">
-                                    <textarea className="form-control form-control-lg" placeholder="Project Description"></textarea>
+                                    <textarea 
+                                        className="form-control form-control-lg" 
+                                        placeholder="Project Description" 
+                                        name='description'
+                                        value={this.state.description}
+                                        onChange={this.onChange}
+                                    />
                                 </div>
                                 <h6>Start Date</h6>
                                 <div className="form-group">
-                                    <input type="date" className="form-control form-control-lg" name="start_date" />
+                                    <input 
+                                        type="date" 
+                                        className="form-control form-control-lg" 
+                                        name="start_date"
+                                        value={this.state.start_date}
+                                        onChange={this.onChange}
+                                    />
                                 </div>
                                 <h6>Estimated End Date</h6>
                                 <div className="form-group">
-                                    <input type="date" className="form-control form-control-lg" name="end_date" />
+                                    <input 
+                                        type="date" 
+                                        className="form-control form-control-lg" 
+                                        name="end_date" 
+                                        value={this.state.end_date}
+                                        onChange={this.onChange}
+                                    />
                                 </div>
 
                                 <input type="submit" className="btn btn-primary btn-block mt-4" />
@@ -61,6 +147,7 @@ const UpdateProjectWrapper = (props) => {
 
 UpdateProjectWrapper.propTypes = {
     getProject: PropTypes.func.isRequired,
+    createProject: PropTypes.func.isRequired,
     project: PropTypes.object.isRequired
 };
 
@@ -70,6 +157,6 @@ const mapStateToProps = state => ({
 
 export default connect(
     mapStateToProps,
-    {getProject}
+    {getProject, createProject}
 )(UpdateProjectWrapper);
 
