8f87386950eca23ff351b62133709ac9c81c5aac
hwlei888 <leihuawen888@gmail.com>
2025-02-04 23:34:27 +1100

    organize project tasks by status and priority

--- src/components/ProjectBoard/Backlog.js
+++ src/components/ProjectBoard/Backlog.js
@@ -11,6 +11,25 @@ class Backlog extends Component {
         />
     ));
 
+    let todoItems = [];
+    let inProgressItems = [];
+    let doneItems = [];
+
+    for(let i = 0; i < tasks.length; i++){
+        console.log(tasks[i]);
+        if(tasks[i].props.project_task.status === "TO_DO"){
+            todoItems.push(tasks[i]);
+        }
+
+        if(tasks[i].props.project_task.status === "IN_PROGRESS"){
+            inProgressItems.push(tasks[i]);
+        }
+
+        if(tasks[i].props.project_task.status === "DONE"){
+            doneItems.push(tasks[i]);
+        }
+    }
+
     return (
         <div className="container">
             <div className="row">
@@ -20,29 +39,25 @@ class Backlog extends Component {
                             <h3>TO DO</h3>
                         </div>
                     </div>
-                    {tasks}
+                    {todoItems}
                 </div>
+
                 <div className="col-md-4">
                     <div className="card text-center mb-2">
                         <div className="card-header bg-primary text-white">
                             <h3>In Progress</h3>
                         </div>
                     </div>
-                    {
-                        //<!-- SAMPLE PROJECT TASK STARTS HERE -->
-                        //<!-- SAMPLE PROJECT TASK ENDS HERE -->
-                    }
+                    {inProgressItems}
                 </div>
+                
                 <div className="col-md-4">
                     <div className="card text-center mb-2">
                         <div className="card-header bg-success text-white">
                             <h3>Done</h3>
                         </div>
                     </div>
-                    {
-                        //<!-- SAMPLE PROJECT TASK STARTS HERE -->
-                        //<!-- SAMPLE PROJECT TASK ENDS HERE -->
-                    }
+                    {doneItems}
                 </div>
             </div>
         </div>
