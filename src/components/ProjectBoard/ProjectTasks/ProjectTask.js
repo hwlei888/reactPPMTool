0d09897cf500adbbf879dc4420bd1d5f7422c45a
hwlei888 <leihuawen888@gmail.com>
2025-02-07 01:56:54 +1100

    delete project task ;

--- src/components/ProjectBoard/ProjectTasks/ProjectTask.js
+++ src/components/ProjectBoard/ProjectTasks/ProjectTask.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import { deleteProjectTask } from '../../../actions/backlogActions';
+import PropTypes from "prop-types";
+import { connect } from 'react-redux';
 
 class ProjectTask extends Component {
+
+    onDeleteClick = (backlog_id, pt_id) => {
+        this.props.deleteProjectTask(backlog_id, pt_id);
+    }
+
     render() {
         let priorityClass;
         let priorityString;
@@ -39,7 +47,14 @@ class ProjectTask extends Component {
                         View / Update
                     </Link>
 
-                    <button className="btn btn-danger ml-4">
+                    <button 
+                        className="btn btn-danger ml-4" 
+                        onClick={this.onDeleteClick.bind(
+                            this,
+                            project_task.projectIdentifier, 
+                            project_task.projectSequence
+                        )}
+                    >
                         Delete
                     </button>
                 </div>
@@ -48,6 +63,13 @@ class ProjectTask extends Component {
     }
 }
 
-export default ProjectTask;
+ProjectTask.propTypes = {
+    deleteProjectTask: PropTypes.func.isRequired,
+}
+
+export default connect(
+    null,
+    {deleteProjectTask}
+)(ProjectTask);
 
 
